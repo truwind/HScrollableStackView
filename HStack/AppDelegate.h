72d0910f7200788f9ac1092af459a4c82829098a
truwind <truwind74@gmail.com>
2017-09-06 18:03:59 +0900

    Initial Commit

---
+++ HStack/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HStack
+//
+//  Created by truwind on 2017. 9. 6..
+//  Copyright © 2017년 truwind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HStack/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HStack
+//
+//  Created by truwind on 2017. 9. 6..
+//  Copyright © 2017년 truwind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
